562bca37afee772409fe0d08fbf5301250b1e331
LakshmiSuvarna <mlsuvarna09@gmail.com>
2026-09-16 09:20:04 +0000

    Fix frontend API base URL syntax

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
+  baseURL:
   import.meta.env.VITE_API_BASE_URL ||
-  baseURL: "http://localhost:8080"
+   "http://localhost:8080"
 });
 
 api.interceptors.request.use(
